9e633447029c1d61e8a1c17421671c04e3d807fc
Daniel Estevez <daniestevez21@gmail.com>
2024-02-25 14:47:06 +0100

    Clase Trabajador.java añadida

---
+++ Trabajador.java
@@ -0,0 +1,20 @@
+public class Trabajador {
+    public static final Object DIRECTOR = "Director";
+    public static final Object SUBDIRECTOR = "Subdirector";
+
+    public Object getTipoTrabajador() {
+        Object tipoTrabajador=0;
+        return tipoTrabajador;
+    }
+
+    public float getNomina() {
+
+        return 0;
+    }
+
+    public float getHorasExtras() {
+
+        return 0;
+    }
+}
+
