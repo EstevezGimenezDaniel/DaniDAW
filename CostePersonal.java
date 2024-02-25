1c68c799bbc482aca00985d38412b91e50d0764c
Daniel Estevez <daniestevez21@gmail.com>
2024-02-25 14:48:06 +0100

    Clase CostePersonal.java añadida

--- CostePersonal.java
+++ CostePersonal.java
@@ -1,18 +1,20 @@
 public class CostePersonal {
- 
-	static float CosteDelPersonal(Trabajador trabajadores[]) {
-		float costeFinal = 0;
-		Trabajador trabajador;
-		for (int i = 0; y < trabajadores.length; i++) {
-			trabajador = trabajadores[i];
-			if (trabajador.getTipoTrabajador() == Trabajador.DIRECTOR|| trabajador.getTipoTrabajador() == Trabajador.SUBDIRECTOR) {
-					costeFinal += trabajador.getNomina();
-			}
-			else
-			{
-				costeFinal += trabajador.getNomina() + (trabajador.getHorasExtras() * 20);
-			}
-		}
-		return costeFinal;
-	}
+    static float CosteDelPersonal(Trabajador trabajadores[]) {
+        float costeFinal = 0;
+        Trabajador trabajador;
+        for (Trabajador trabajador1 : trabajadores) {
+            trabajador = trabajador1;
+            if (trabajador.getTipoTrabajador() == Trabajador.DIRECTOR || trabajador.getTipoTrabajador() == Trabajador.SUBDIRECTOR) {
+                costeFinal += trabajador.getNomina();
+            } else {
+                costeFinal = getCosteFinal(costeFinal, trabajador);
+            }
+        }
+        return costeFinal;
+    }
+
+    private static float getCosteFinal(float costeFinal, Trabajador trabajador) {
+        costeFinal += trabajador.getNomina() + (trabajador.getHorasExtras() * 20);
+        return costeFinal;
+    }
 }
